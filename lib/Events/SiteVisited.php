d2585f11dce676f18f3fcd9e874c1a1cd903d632
Alex Standiford <alex@standiford.us>
2023-11-17 08:20:03 -0600

    progress

--- lib/Events/SiteVisited.php
+++ lib/Events/SiteVisited.php
@@ -6,8 +6,20 @@ use PHPNomad\Events\Interfaces\Event;
 
 class SiteVisited implements Event
 {
+    protected ?int $visitorId;
+
+    public function __construct(?int $visitorId)
+    {
+        $this->visitorId = $visitorId;
+    }
+
     public static function getId(): string
     {
         return 'site_visited';
+    }
+
+    public function getVisitorId(): int
+    {
+        return $this->visitorId;
     }
 }
